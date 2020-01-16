d1a2921a91d3221db6105e89c70cd74cf1b0223f
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 16:25:53 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
 cd %~dp0
-dotnet build magic.lambda.hyperlambda/magic.lambda.hyperlambda.csproj --configuration Release
+dotnet build magic.lambda.hyperlambda/magic.lambda.hyperlambda.csproj --configuration Release --source https://api.nuget.org/v3/index.json
